f8061024b8866660d33024f62099ddd299167c8b
shreyash jain <sheryash123jain@gmail.com>
2024-03-06 18:53:53 +0530

    u

--- frontend/src/SignUp.jsx
+++ frontend/src/SignUp.jsx
@@ -42,43 +42,11 @@ const SignUp = ()=>{
         <>
         
 
-        {/* <div className="body">
-        <div class="background">
-        <div class="shape"></div>
-        <div class="shape"></div>
-    </div>
-    <form className="form">
-        <h3 >Sign Up</h3>
-
-        <label className="label" for="username">Full Name</label>
-        <input  className="input" type="text" onChange={setdata} name="Name" placeholder="Full Name" id="username"/>
-        <label className="label" for="username">Email*</label>
-        <input className="input" type="email" onChange={setdata} required name="Email" placeholder="Email" id="username"/>
-
-        <label className="label"  for="password">Password*</label>
-        <input className="input" type="password" name="Password" required onChange={setdata} placeholder="Password" id="password"/>
-        {loading ? (
-                      <div className="flex mt-10 justify-evenly ">
-                     <DotSpinner size={40} speed={0.9} color="white" className="flex items-center  justify-center mx-auto" />
-                     <DotSpinner size={40} speed={0.9} color="red" className="flex items-center  justify-center mx-auto" />
-                     <DotSpinner size={40} speed={0.9} color="blue" className="flex items-center  justify-center mx-auto" />
-                 </div>
-                  ) : (
-                    <button className="button" onClick={savedata}>Sign Up</button> 
-                  )}
-        <div class="social">
-          <div class="go"><i class="fab fa-google"></i>  Google</div>
-          <div class="fb"><Link to={"/login"} class="fab fa-facebook">Sign In</Link></div>
-        </div>    </form>
-
-    
-        </div> */}
-
 <div className="main_root">
 
 <div class="main">  	
     <div class="signup">
-            <form method="POST">
+            <form onSubmit={savedata} method="POST">
                 <label className="label" for="chk" aria-hidden="true">Sign Up</label>
                 <input  className="sign_input" type="text" onChange={setdata} name="Name" placeholder="Full Name" required/>
                 <input  className="sign_input" type="email" onChange={setdata} required name="Email" placeholder="Email" />
@@ -91,7 +59,7 @@ const SignUp = ()=>{
                      <DotSpinner size={40} speed={0.9} color="blue" className="flex items-center  justify-center mx-auto" />
                  </div>
                   ) : (
-                    <button className="signup_button bg-purple-600 text-white" onClick={savedata}>Sign Up</button> 
+                    <input type="submit" className="signup_button bg-purple-600 text-white" >Sign Up</input> 
                   )}
     </form>   
         </div>
